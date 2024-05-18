91d43461a416c21a0b6a8a591eb25ce3d72a5e48
Aaron Gazzola <aaron@gazzola.dev>
2024-05-18 21:18:42 +1000

    feature(Results): add home button

--- app/ui/Results.tsx
+++ app/ui/Results.tsx
@@ -5,9 +5,11 @@ import { UserWithAnswers } from "../lib/types";
 import { prepareDataForSpiderChart } from "../lib/util/d3";
 import SpiderChart from "./SpiderChart";
 import Histogram from "./Histogram";
-import { Typography } from "antd";
+import { Button, Typography } from "antd";
 import useIsMounted from "../lib/hooks/useIsMounted";
 import Loading from "../results/[user_id]/loading";
+import { LeftOutlined } from "@ant-design/icons";
+import Link from "next/link";
 
 const Results = ({
   users,
@@ -40,6 +42,11 @@ const Results = ({
           users={users}
           user_id={user_id}
         />
+      </div>
+      <div className="p-4">
+        <Link href="/">
+          <Button icon={<LeftOutlined />}>Home</Button>
+        </Link>
       </div>
     </div>
   );
