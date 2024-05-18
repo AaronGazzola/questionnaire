cf8c9eec77a65965ec754649d3c1d945502305b6
Aaron Gazzola <aaron@gazzola.dev>
2024-05-18 21:11:45 +1000

    feature(Results): add loading skeleton for results page

--- app/results/[user_id]/loading.tsx
+++ app/results/[user_id]/loading.tsx
@@ -1,27 +1,20 @@
+"use client";
 import React from "react";
 import SkeletonInput from "antd/es/skeleton/Input";
-import SkeletokButton from "antd/es/skeleton/Button";
+import { Skeleton } from "antd";
 
 export default function Loading() {
   return (
-    <div className="h-screen flex flex-col justify-between">
-      <div className="p-5 flex flex-col flex-1 justify-center items-center space-y-5">
-        <div className="w-10/12">
-          <SkeletonInput
-            active
-            block
-          />
-        </div>
-        <div className="w-8/12 sm:w-96">
-          <SkeletonInput
-            active
-            block
-          />
-        </div>
+    <div className="flex flex-col justify-center items-center">
+      <div className="w-56 p-7">
+        <SkeletonInput
+          active
+          block
+        />
       </div>
-      <div className="flex justify-between p-4">
-        <SkeletokButton active />
-        <SkeletokButton active />
+      <div className="flex flex-wrap justify-center">
+        <Skeleton.Node style={{ width: 548, height: 726, margin: "1rem" }} />
+        <Skeleton.Node style={{ width: 448, height: 641, margin: "1rem" }} />
       </div>
     </div>
   );
